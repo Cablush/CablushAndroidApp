cd486f31c15619d868688d19681a8e4808943306
jhonnybat <jhonnybat@gmail.com>
2015-10-25 12:25:54 -0200

    Ajustes da action bar e inicio da criaçao do dialog buscar

--- app/src/main/java/com/cablush/cablushandroidapp/MainActivity.java
+++ app/src/main/java/com/cablush/cablushandroidapp/MainActivity.java
@@ -1,11 +1,15 @@
 package com.cablush.cablushandroidapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -19,6 +23,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+
         setContentView(R.layout.activity_main);
          getActionBar().show();
         // getBundle();
@@ -52,8 +58,8 @@ public class MainActivity extends Activity {
                             getString(R.string.error_create_map), Toast.LENGTH_SHORT).show();
                 }
 
-                // LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                // Criteria criteria = new Criteria();
+                 LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                 Criteria criteria = new Criteria();
 
             }
         } catch (NullPointerException e){
@@ -74,6 +80,11 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
+        switch (id){
+            case R.id.action_search:
+
+                break;
+        }
 
 
         return super.onOptionsItemSelected(item);
