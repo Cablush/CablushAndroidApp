c853c02b0bc0f1c40860ca53d439ef86f3129c09
Oscar Costa <oscarcosta@gmail.com>
2016-02-04 22:13:30 -0300

    Icones para o local info

--- app/src/main/java/com/cablush/cablushapp/view/dialogs/LocalInfoDialog.java
+++ app/src/main/java/com/cablush/cablushapp/view/dialogs/LocalInfoDialog.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cablush.cablushapp.R;
@@ -61,14 +62,18 @@ public class LocalInfoDialog<L extends Localizavel> extends DialogFragment {
         descricao.setText(mLocalizavel.get().getDescricao());
 
         TextView telefone = (TextView) view.findViewById(R.id.telefoneTextView);
+        ImageView telefoneIcon = (ImageView) view.findViewById(R.id.imageViewTelefone);
         TextView email = (TextView) view.findViewById(R.id.emailTextView);
+        ImageView emailIcon = (ImageView) view.findViewById(R.id.imageViewEmail);
         if (mLocalizavel.get() instanceof Loja) {
             Loja loja = (Loja) mLocalizavel.get();
             telefone.setText(loja.getTelefone());
             email.setText(loja.getEmail());
         } else {
             telefone.setVisibility(View.GONE);
+            telefoneIcon.setVisibility(View.GONE);
             email.setVisibility(View.GONE);
+            emailIcon.setVisibility(View.GONE);
         }
 
         // TODO facebook & site & directions && esportes
