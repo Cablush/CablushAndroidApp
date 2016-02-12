7313d9a6be4668c2e80ff63e1f3fbfbc0c2bf576
Oscar Costa <oscarcosta@gmail.com>
2016-02-11 22:15:41 -0300

    Config Changes;

--- app/src/main/java/com/cablush/cablushapp/CablushApp.java
+++ app/src/main/java/com/cablush/cablushapp/CablushApp.java
@@ -12,6 +12,6 @@ public class CablushApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FontCache.overrideFont(this, "normal", "fonts/PassionOne-Bold.ttf");
+        //FontCache.overrideFont(this, "normal", "fonts/PassionOne-Bold.ttf");
     }
 }
